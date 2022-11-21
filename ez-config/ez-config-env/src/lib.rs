fda149b2a1dd222e41df107cb654aef20e40edce
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-11-21 13:26:03 +0100

    implement happy path

--- ez-config/ez-config-env/src/lib.rs
+++ ez-config/ez-config-env/src/lib.rs
@@ -17,7 +17,10 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let output = quote! {
         impl #ident {
             // #[doc = #msg]
-            fn from_env() {
+            fn from_env() -> Self {
+                #ident {
+                    #(#nested_output),*
+                }
             }
         }
     };
@@ -31,26 +34,15 @@ fn generate_from_env(data: &Data) -> Vec<TokenStream> {
             match data.fields {
                 Fields::Named(ref fields) => {
                     println!("fields are {:#?}", fields.named);
-                    // Expands to an expression like
-                    //
-                    //     0 + self.x.heap_size() + self.y.heap_size() + self.z.heap_size()
-                    //
-                    // but using fully qualified function call syntax.
-                    //
-                    // We take some care to use the span of each `syn::Field` as
-                    // the span of the corresponding `heap_size_of_children`
-                    // call. This way if one of the field types does not
-                    // implement `HeapSize` then the compiler's error message
-                    // underlines which field it is. An example is shown in the
-                    // readme of the parent directory.
+
                     let result: Vec<TokenStream> = fields.named.iter().map(|f| {
                         let name = &f.ident;
-
-                        let isString = if let syn::Type::Path(ref p) = f.ty {
+                        let env_variable = format!("{}", name.as_ref().unwrap().to_string());
+                        let is_string = if let syn::Type::Path(ref p) = f.ty {
                             p.path.segments.len() == 1 && p.path.segments[0].ident == "String"
                         } else { false };
                         quote! {
-                            println!("Is string? {}", #isString)
+                            #name: ::std::env::var(#env_variable).unwrap()
                         }
                     }).collect();
                     result
